4f499107a07c26bf2a0267ba435647ba34d0e177
mike <miked@macs-mbp.home>
2019-01-19 19:43:42 -0500

    change to prod public api endpoint

--- gemini_connection.py
+++ gemini_connection.py
@@ -13,7 +13,7 @@ import re
 
 http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
 urllib3.contrib.pyopenssl.inject_into_urllib3()
-get_book = "https://api.sandbox.gemini.com/v1/book/"
+get_book = "https://api.gemini.com/v1/book/"
 
 def autocapitalize(event):
     crypto.set(crypto.get().upper())
@@ -30,13 +30,16 @@ def check_updown(price,old_price):
 old_value = 0
 
 def getMarketData():
+    localtime = time.asctime( time.localtime(time.time()))
+    texttime.delete(0,30)
+    texttime.insert(INSERT,localtime)
     textbid.config(state=NORMAL)
     textask.config(state=NORMAL)
     textbid.delete(1.0,END)
     textask.delete(1.0,END)
     request_symbols = []
     btc_book = {}
-    base_url = "https://api.sandbox.gemini.com/v1/symbols"
+    base_url = "https://api.gemini.com/v1/symbols"
     r_symbols = http.request('GET', base_url)
     r = http.request('GET', get_book)
     request_symbols = r_symbols.data.decode('utf-8')
@@ -49,25 +52,21 @@ def getMarketData():
     print("BIDs -------------------------")
     for x in btc_book['bids']:
         bids.append(x)
-    print(bids[0]['price'])
-    textbid.insert(INSERT,bids[0]['price'].strip())
+    while i < len(bids):
+        #textbid.insert(INSERT,bids[i]['price']+"\n")
+        textbid.insert(INSERT,bids[i]['timestamp']+" "+bids[i]['amount']+" "+bids[i]['price']+"\n")
+        i += 1
 
     i = 0
     asks = []
     print("ASKs -------------------------")
     for x in btc_book['asks']:
         asks.append(x)
-    print(asks)
-   # i = 0
-   # asks = []
-   # print("ASKs -------------------------")
-   # for x in btc_book['asks']:
-   #     while i < len(x):
-   #         asks.append(btc_book['asks'][i])
-   #         print(btc_book['asks'][i])
-   #         i += 1
-    #print(request_symbols)
-    #print(r.data)
+    while i < len(asks):
+        #textask.insert(INSERT,asks[i]['price']+"\n")
+        #textask.insert(INSERT,asks[i])
+        textask.insert(INSERT,asks[i]['timestamp']+" "+asks[i]['amount']+" "+asks[i]['price']+"\n")
+        i += 1
 
 
 
@@ -86,8 +85,8 @@ equity = Entry(top, width=5, textvariable=crypto)
 textlt=Entry(top, width=25,background='black',foreground='red')
 texttime=Entry(top, width=25, background ='black', foreground='red')
 refresh = Button(top, text="Refresh", command=getMarketData)
-textbid=Text(top, width=20, height=20, background ='black', foreground='green')
-textask=Text(top, width=20, height=20, background ='black', foreground='green')
+textbid=Text(top, width=60, height=60, background ='black', foreground='green')
+textask=Text(top, width=60, height=60, background ='black', foreground='green')
 equity.pack()
 equity.bind("<KeyRelease>",autocapitalize)
 textlt.pack()
